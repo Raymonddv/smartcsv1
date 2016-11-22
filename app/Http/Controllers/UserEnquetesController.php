c502433d0ef0d2b4400099b989ca15bce073c77c
Raymonddv <raymond_max@hotmail.com>
2016-11-22 16:54:38 +0100

    enquete scoring restaurant 1

--- app/Http/Controllers/UserEnquetesController.php
+++ app/Http/Controllers/UserEnquetesController.php
@@ -1,18 +1,54 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UserEnquetes;
+use DB;
 
 class UserEnquetesController extends Controller
 {
-         public function index()
-      {
+    public function index()
+    {
         return view('UserEnquete');
-      }
+    }
 
-      public function store()
-        {
+    public function store(Request $request)
+    {
+       DB::table('user_enquetes')
+           ->where('id', 1)
+           ->update(['place_1_kwaliteit_eten' => $request->input('place_1_kwaliteit_eten')]);
 
-        }
+           DB::table('user_enquetes')
+           ->where('id', 1)
+           ->update(['place_1_prijs_kwaliteit' => $request->input('place_1_prijs_kwaliteit')]);
+
+           DB::table('user_enquetes')
+           ->where('id', 1)
+           ->update(['place_1_variatie' => $request->input('place_1_variatie')]);
+
+           DB::table('user_enquetes')
+           ->where('id', 1)
+           ->update(['place_1_sfeer' => $request->input('place_1_sfeer')]);
+
+           DB::table('user_enquetes')
+           ->where('id', 1)
+           ->update(['place_1_service' => $request->input('place_1_service')]);
+
+
+   }
 }
+
+         // $enquete = new UserEnquetes;
+          //$enquete->place_1_kwaliteit_eten = ('place_1_kwaliteit_eten');
+
+
+
+
+         // $user = new User;
+          //$user->username = Input::get('username');
+          //$user->email = Input::get('email');
+          //$user->password = Hash::make(Input::get('password'));
+          //$user->save();
+
+    //return Redirect::back();
+
--- resources/views/userenquete.blade.php
+++ resources/views/userenquete.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
-
 @section('content')
 <div class="container">
-<h1>SWAGH</h1>
-<!-- slider 1 -->
+<h1>Vraag</h1>
+<!-- slider 1
 
-     <div class="sliderContainer" name="place_1_kwaliteit_eten">
+      <div class="sliderContainer" name="place_1_kwaliteit_eten">
        <input type="checkbox" id="checkbox-1" class="regular-checkbox" onclick="disable()">
        <label for="checkbox-1" style="top: 5px"></label>
        <label class="question" for="checkbox-1">Goede kwaliteit van het eten
@@ -17,6 +16,7 @@
        <p class="sliderText neutraalT">Neutraal</p>
        <p class="sliderText goedT">Goed</p>
        <p class="sliderText uitstekendT">Uitstekend</p>
+
      </div>
      <script>
        function disable(){
@@ -30,4 +30,25 @@
      </script>
       <input type="submit" value="Submit">
      </div>
+-->
+<div class="container">
+  <form method="post" action="{{ action('UserEnquetesController@store') }}">
+    <input type="range" name="place_1_kwaliteit_eten" max="11" min="0" step="1" value="">
+    <br>
+    <input type="range" name="place_1_prijs_kwaliteit" max="11" min="0" step="1" value="">
+    <br>
+    <input type="range" name="place_1_variatie" max="11" min="0" step="1" value="">
+    <br>
+    <input type="range" name="place_1_sfeer" max="11" min="0" step="1" value="">
+    <br>
+    <input type="range" name="place_1_service" max="11" min="0" step="1" value="">
+    <br>
+    <input type="submit" value="Submit">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+  </form>
+</div>
+
+
+
+
 @stop
