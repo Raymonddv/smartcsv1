29c496430ebde205d57a25503c5baabd4f54d268
Jamushroom <jamushmail@gmail.com>
2016-11-23 13:31:30 +0100

    Add vragen HTML

--- resources/views/vragen.blade.php
+++ resources/views/vragen.blade.php
@@ -4,33 +4,58 @@
 @stop
 
 @section('content')
-<div class="container">
-    <h1>Vragen Restaurant</h1>
+@extends('layouts.app')
 
-        Maak hier uw keuze uit uw 3 favoriete restaurants.
+@section('header')
+@stop
 
-  <form action= {{ action('VragenController@store') }} method="post">
-    <select name="place_1">
-      @foreach ($places as $place)
-      <option value="{{$place->name}}">{{$place->name}}</option>
-      @endforeach
-    </select>
+@section('content')
+  <div class="mainWrapper">
+    <h1 class="subPageTitle">De Smaak van Leeuwarden</h1>
+    <h3 id="choiceTitle">Maak hieronder een keuze van de 3 beste locale restaurants:</h3>
+    <section id="bestRestaurantsSection">
 
-    <select name="place_2">
-      @foreach ($places as $place)
-      <option value="{{$place->name}}">{{$place->name}}</option>
-      @endforeach
-    </select>
 
-    <select name="place_3">
-      @foreach ($places as $place)
-      <option value="{{$place->name}}">{{$place->name}}</option>
-      @endforeach
-    </select>
-    <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-    <button type="submit">Verzend</button>
-  </form>
-</div>
+      <h6>Keuze 1</h6>
+      <select name="choice1" id="choice1"/>
+        <option value="" hidden="hidden">Maak een keuze</option>
+        <option value="1">Restaurant 1</option>
+        <option value="2">Restaurant 2</option>
+        <option value="3">Restaurant 3</option>
+        <option value="4">Restaurant 4</option>
+        <option value="5">Restaurant 5</option>
+        <option value="6">Restaurant 6</option>
+        <option value="7">Restaurant 7</option>
+        <option value="8">Restaurant 8</option>
+      </select>
+
+      <h6>Keuze 2</h6>
+      <select name="choice2" id="choice2">
+        <option value="" hidden="hidden">Maak een keuze</option>
+        <option value="1">Link to database</option>
+      </select>
+
+      <h6>Keuze 3</h6>
+      <select name="choice3" id="choice3">
+        <option value="" hidden="hidden">Maak een keuze</option>
+        <option value="1">Link to database</option>
+      </select>
+    </section>
+    <div class="buttonCenter">
+      <a href="#">
+        <input type="button" class="button gradientcolor" title="button"
+        value="Volgende vraag">
+      </a>
+    </div>
+  </div>
+
+
+
+
+
+
+@stop
+
 
 
 
