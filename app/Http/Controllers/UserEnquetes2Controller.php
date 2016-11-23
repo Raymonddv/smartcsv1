5f949e7e311790a92748e40dfac4af7550ed00ad
Raymonddv <raymond_max@hotmail.com>
2016-11-23 09:26:23 +0100

    pagina 2 enquete on user id

---
+++ app/Http/Controllers/UserEnquetes2Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
+
+class UserEnquetes2Controller extends Controller
+{
+    public function index()
+    {
+        return view('UserEnquete2');
+    }
+
+    public function store(Request $request)
+    {
+      $id=Auth::id();
+
+       DB::table('user_enquetes')
+           ->where('id', $id)
+           ->update(['place_2_kwaliteit_eten' => $request->input('place_2_kwaliteit_eten')]);
+
+           DB::table('user_enquetes')
+           ->where('id', $id)
+           ->update(['place_2_prijs_kwaliteit' => $request->input('place_2_prijs_kwaliteit')]);
+
+           DB::table('user_enquetes')
+           ->where('id', $id)
+           ->update(['place_2_variatie' => $request->input('place_2_variatie')]);
+
+           DB::table('user_enquetes')
+           ->where('id', $id)
+           ->update(['place_2_sfeer' => $request->input('place_2_sfeer')]);
+
+           DB::table('user_enquetes')
+           ->where('id', $id)
+           ->update(['place_2_service' => $request->input('place_2_service')]);
+
+           return redirect('home');
+   }
+}
+
--- app/Http/Controllers/UserEnquetesController.php
+++ app/Http/Controllers/UserEnquetesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\UserEnquetes;
 use DB;
+use Illuminate\Support\Facades\Auth;
 
 class UserEnquetesController extends Controller
 {
@@ -14,24 +15,26 @@ class UserEnquetesController extends Controller
 
     public function store(Request $request)
     {
+      $id=Auth::id();
+
        DB::table('user_enquetes')
-           ->where('id', 1)
+           ->where('id', $id)
            ->update(['place_1_kwaliteit_eten' => $request->input('place_1_kwaliteit_eten')]);
 
            DB::table('user_enquetes')
-           ->where('id', 1)
+           ->where('id', $id)
            ->update(['place_1_prijs_kwaliteit' => $request->input('place_1_prijs_kwaliteit')]);
 
            DB::table('user_enquetes')
-           ->where('id', 1)
+           ->where('id', $id)
            ->update(['place_1_variatie' => $request->input('place_1_variatie')]);
 
            DB::table('user_enquetes')
-           ->where('id', 1)
+           ->where('id', $id)
            ->update(['place_1_sfeer' => $request->input('place_1_sfeer')]);
 
            DB::table('user_enquetes')
-           ->where('id', 1)
+           ->where('id', $id)
            ->update(['place_1_service' => $request->input('place_1_service')]);
 
            return redirect('home');
